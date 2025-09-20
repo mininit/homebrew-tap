79f46029c23cb7296877e05fb5cb715e851fe055
mininit <50385867+mininit@users.noreply.github.com>
2025-09-20 13:52:33 +1000

    chore: bump imgbase version to v0.4.0

--- imgbase.rb
+++ imgbase.rb
@@ -1,9 +1,9 @@
 class Imgbase < Formula
-  desc "CLI tool to encode images as base64 data URIs"
+  desc "Encode images as base64 data URIs"
   homepage "https://github.com/mininit/imgbase"
   url "https://github.com/mininit/imgbase.git",
-      tag:      "v0.3.0",
-      revision: "82fc074d0ddacdcdc791e0e9e5785b230e1ab177"
+      tag:      "v0.4.0",
+      revision: "d2648caa9d4bb510744e9d0948327f4fff41c899"
   license "MIT"
   head "https://github.com/mininit/imgbase.git", branch: "main"
 
