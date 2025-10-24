933017712c35621a17870d18da0ac539ef1ac32d
mininit <50385867+mininit@users.noreply.github.com>
2025-10-24 22:39:05 +1000

    chore: bump nav.rb to v0.3.0

--- nav.rb
+++ nav.rb
@@ -2,8 +2,8 @@ class Nav < Formula
   desc "Terminal-based file navigator"
   homepage "https://github.com/mininit/nav"
 
-  url "https://github.com/mininit/nav/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "18fb7dc724b10be7242c07cb740e1a6b6a7e462e2381c0bf330a24c373d5b188"
+  url "https://github.com/mininit/nav/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "b5f617aa721e2028300f01208c265e12c9e87720188f7e8a86c8d4100dbe3f40"
   license "GPL-3.0-only"
 
   head "https://github.com/mininit/nav.git", branch: "main"
