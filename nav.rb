dc6d2189b1da48d31104e5727a40dce571b8b028
mininit <50385867+mininit@users.noreply.github.com>
2025-10-24 22:47:08 +1000

    chore: bump nav.rb to v0.3.0

--- nav.rb
+++ nav.rb
@@ -3,7 +3,7 @@ class Nav < Formula
   homepage "https://github.com/mininit/nav"
 
   url "https://github.com/mininit/nav/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "b5f617aa721e2028300f01208c265e12c9e87720188f7e8a86c8d4100dbe3f40"
+  sha256 "8bf5cda0f5c0770e111416fe4261b2537c8c181248213da2e74ba3cf9e7ca194"
   license "GPL-3.0-only"
 
   head "https://github.com/mininit/nav.git", branch: "main"
