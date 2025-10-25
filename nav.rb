581b0dca276910e6f1d42138bee5ae74e7b365f5
mininit <50385867+mininit@users.noreply.github.com>
2025-10-25 11:20:26 +1000

    chore: bump imgbase version to v0.3.0

--- nav.rb
+++ nav.rb
@@ -1,21 +1,19 @@
 class Nav < Formula
   desc "Terminal-based file navigator"
   homepage "https://github.com/mininit/nav"
-
   url "https://github.com/mininit/nav/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "8bf5cda0f5c0770e111416fe4261b2537c8c181248213da2e74ba3cf9e7ca194"
+  sha256 "1ef7d397160d61681edbb0912403248b6926275a0bddd2b77b40b145e492903c"
   license "GPL-3.0-only"
-
   head "https://github.com/mininit/nav.git", branch: "main"
 
   depends_on "ncurses"
 
   def install
-    system "make"
-    bin.install "nav"
+    system "make", "install" 
   end
 
   test do
-    assert_match "nav version #{version}", shell_output("#{bin}/nav -v")
+    output = shell_output("#{bin}/nav -v")
+    assert_match version.to_s, output
   end
 end
