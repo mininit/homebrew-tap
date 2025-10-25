74fcdf3deb5da8e9438fd361b3eefb5c7cde8189
mininit <50385867+mininit@users.noreply.github.com>
2025-10-25 11:35:11 +1000

    chore: update nav.rb

--- nav.rb
+++ nav.rb
@@ -2,7 +2,7 @@ class Nav < Formula
   desc "Terminal-based file navigator"
   homepage "https://github.com/mininit/nav"
   url "https://github.com/mininit/nav/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "1ef7d397160d61681edbb0912403248b6926275a0bddd2b77b40b145e492903c"
+  sha256 "ace9e3463abf641527833f3c4bff91931aa9dc726e6f8feaaccb4eea6b19463a"
   license "GPL-3.0-only"
   head "https://github.com/mininit/nav.git", branch: "main"
 
