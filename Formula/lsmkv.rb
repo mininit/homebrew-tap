4f71faefbc8a0d2f2d54ba41190eaa0e05808e0f
mininit <dev@mininit.com>
2026-08-18 15:57:39 +1000

    chore: bump version to v0.1.1
    Signed-off-by: mininit <dev@mininit.com>

--- Formula/lsmkv.rb
+++ Formula/lsmkv.rb
@@ -1,8 +1,8 @@
 class Lsmkv < Formula
   desc "A CLI tool for auditing Matroska (.mkv) files"
   homepage "https://github.com/mininit/lsmkv"
-  url "https://github.com/mininit/lsmkv/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "e75ebb0766133f908df3ad5f4653a92f402d4952cb36a3ebf0368d4cba8248d9"
+  url "https://github.com/mininit/lsmkv/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "1d6a7e11b4fb9e974bf177c2d9c50660044709bea9d66c2440e27b0b5fe52dfb"
   license "MIT"
 
   depends_on "rust" => :build
